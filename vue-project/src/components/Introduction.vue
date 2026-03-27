de5680df62e081baa2698ce2e012486e198d6066
Grant King <grantryan.king@gmail.com>
2026-03-27 02:12:15 -0700

    Update Introduction.vue
    Add special formatting area for Baum's signature.
    
    Add padding to text within the dedication plaque.
    
    Also format the entire .vue document for proper whitespace, linebreaks, and indentation.

--- vue-project/src/components/Introduction.vue
+++ vue-project/src/components/Introduction.vue
@@ -1,63 +1,74 @@
 <template>
 
     <div>
-Introducing this Story
-</div>
-<div>
-Here is a story with a boy hero, and a boy of whom you have never before heard. There are girls in the story, too, including our old friend Dorothy, and some of the characters wander a good way from the Land of Oz before they all assemble in the Emerald City to take part in Ozma's banquet. Indeed, I think you will find this story quite different from the other histories of Oz, but I hope you will not like it the less on that account.
-</div>
-<div>
-If I am permitted to write another Oz book it will tell of some thrilling adventures encountered by Dorothy, Betsy Bobbin, Trot and the Patchwork Girl right in the Land of Oz, and how they discovered some amazing creatures that never could have existed outside a fairy-land. I have an idea that about the time you are reading this story of Rinkitink I shall be writing that story of Adventures in Oz.
-</div>
-<div>
-Don't fail to write me often and give me your advice and suggestions, which I always appreciate. I get a good many letters from my readers, but every one is a joy to me and I answer them as soon as I can find time to do so.
-</div>
-<div>
-"OZCOT"
-</div>
-<div>
-     at HOLLYWOOD
-</div>
-<div>
-         in CALIFORNIA, 1916.
-</div>
-<div>
-L. FRANK BAUM
-</div>
-<div>
-     Royal Historian of Oz
-</div>
+        Introducing this Story
+    </div>
+    <div>
+        Here is a story with a boy hero, and a boy of whom you have never before heard. There are girls in the story,
+        too, including our old friend Dorothy, and some of the characters wander a good way from the Land of Oz before
+        they all assemble in the Emerald City to take part in Ozma's banquet. Indeed, I think you will find this story
+        quite different from the other histories of Oz, but I hope you will not like it the less on that account.
+    </div>
+    <div>
+        If I am permitted to write another Oz book it will tell of some thrilling adventures encountered by Dorothy,
+        Betsy Bobbin, Trot and the Patchwork Girl right in the Land of Oz, and how they discovered some amazing
+        creatures that never could have existed outside a fairy-land. I have an idea that about the time you are reading
+        this story of Rinkitink I shall be writing that story of Adventures in Oz.
+    </div>
+    <div>
+        Don't fail to write me often and give me your advice and suggestions, which I always appreciate. I get a good
+        many letters from my readers, but every one is a joy to me and I answer them as soon as I can find time to do
+        so.
+    </div>
+    <div>
+        <div>
+            "OZCOT"
+        </div>
+        <div>
+            at HOLLYWOOD
+        </div>
+        <div>
+            in CALIFORNIA, 1916.
+        </div>
+        <div class="h-4"></div>
+        <div class="font-semibold">
+            L. FRANK BAUM
+        </div>
+        <div>
+            Royal Historian of Oz
+        </div>
+    </div>
 
 
     <div class="h-32"></div>
     <div class="max-w-2xl mx-auto w-full">
         <div class="border-4 border-black/40 rounded">
             <div
-                class="grid grid-cols-1 gap-2 text-xl font-semibold text-center text-white font-serif bg-black/40 rounded py-16 border-16 border-double border-white/80">
+                class="p-2 grid grid-cols-1 gap-2 text-xl font-semibold text-center text-white font-serif bg-black/40 rounded py-16 border-16 border-double border-white/80">
                 <div>
-Wherein is recorded the Perilous Quest of
-</div>
-<div>
-Prince Inga of Pingaree and King
-</div>
-<div>
-Rinkitink in the Magical
-</div>
-<div>
-Isles that lie beyond
-</div>
-<div>
-the Borderland
-</div>
-<div>
-of Oz
-</div>
-<div>
-By L. Frank Baum
-</div>
-<div>
-"Royal Historian of Oz"
-</div>
+                    Wherein is recorded the Perilous Quest of
+                </div>
+                <div>
+                    Prince Inga of Pingaree 
+                </div>
+                <div>
+                    and 
+                </div>
+                <div>
+                    King Rinkitink 
+                </div>
+                <div>
+                    in the Magical Isles 
+                </div>
+                <div>
+                    that lie beyond the Borderland of Oz
+                </div>
+                <div>
+                    By L. Frank Baum
+                </div>
+                <div>
+                    "Royal Historian of Oz"
+                </div>
 
             </div>
         </div>
